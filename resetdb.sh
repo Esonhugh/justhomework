7efa7a8dcabbfffc0301ee5432a2697123214d33
esonhugh <esonhughoutside@gmail.com>
2021-09-27 17:26:19 +0800

    resetdb shell file

--- resetdb.sh
+++ resetdb.sh
@@ -1,15 +1,18 @@
-export url="http://127.0.0.1:5000"
-curl "$url/reinitdb"
-curl "$url/" -v
+export url="http://127.0.0.1:5000/"
+curl "$url/reinitdb" -X DELETE
+echo "restart db !"
 sleep 2s
-curl "$url/uploadData" -d "user=1&image_url=User1"
-curl "$url/uploadData" -d "user=2&image_url=User2"
-curl "$url/uploadData" -d "user=3&image_url=User3"
-curl "$url/uploadData" -d "user=4&image_url=User4"
-curl "$url/uploadData" -d "user=5&image_url=User5"
-curl "$url/uploadData" -d "user=6&image_url=User6"
-curl "$url/uploadData" -d "user=7&image_url=User7"
-curl "$url/uploadData" -d "user=8&image_url=User8"
+curl "$url/" -v
+echo "api usage"
+sleep 2s
+curl "$url/userdata" -d "user=1&image_url=User1"
+curl "$url/userdata" -d "user=2&image_url=User2"
+curl "$url/userdata" -d "user=3&image_url=User3"
+curl "$url/userdata" -d "user=4&image_url=User4"
+curl "$url/userdata" -d "user=5&image_url=User5"
+curl "$url/userdata" -d "user=6&image_url=User6"
+curl "$url/userdata" -d "user=7&image_url=User7"
+curl "$url/userdata" -d "user=8&image_url=User8"
 echo "input user success"
 sleep 2s
 
