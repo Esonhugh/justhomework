ed91606ca941c0dfff2ddbdc81a3a9a0dccc31bd
esonhugh <esonhughoutside@gmail.com>
2021-09-29 21:17:07 +0800

    docker run!!and deploy!

--- resetdb.sh
+++ resetdb.sh
@@ -1,18 +1,17 @@
-export url="http://127.0.0.1:5000/"
-curl "$url/reinitdb" -X DELETE
-echo "restart db !"
-sleep 2s
+export url="http://kali.esonhugh.me:8080/"
 curl "$url/" -v
 echo "api usage"
 sleep 2s
-curl "$url/userdata" -d "user=1&image_url=User1"
-curl "$url/userdata" -d "user=2&image_url=User2"
-curl "$url/userdata" -d "user=3&image_url=User3"
-curl "$url/userdata" -d "user=4&image_url=User4"
-curl "$url/userdata" -d "user=5&image_url=User5"
-curl "$url/userdata" -d "user=6&image_url=User6"
-curl "$url/userdata" -d "user=7&image_url=User7"
-curl "$url/userdata" -d "user=8&image_url=User8"
+curl "$url/userdata" -d "user=1&image_url=User1_image"
+curl "$url/userdata" -d "user=2&image_url=User2_image"
+curl "$url/userdata" -d "user=3&image_url=User3_image"
+curl "$url/userdata" -d "user=4&image_url=User4_image"
+curl "$url/userdata" -d "user=5&image_url=User5_image"
+curl "$url/userdata" -d "user=6&image_url=User6_image"
+curl "$url/userdata" -d "user=7&image_url=User7_image"
+curl "$url/userdata" -d "user=8&image_url=User8_image"
+curl "$url/userdata" -d "user=9&image_url=User9_image"
+curl "$url/userdata" -d "user=10&image_url=User10_image"
 echo "input user success"
 sleep 2s
 
